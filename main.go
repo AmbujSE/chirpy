ca6b953f63ef6b5cf54d50aab510e11ac98be428
Ambujse <blastedaway94@gmail.com>
2026-05-30 07:11:33 +0000

    A: Made the server run with the index file

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"net/http"
 )
 
@@ -12,13 +12,12 @@ func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/api/", apiHandler{})
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		// The "/" pattern matches everything, so we need to check
-		// that we're at the root here.
-		if req.URL.Path != "/" {
-			http.NotFound(w, req)
-			return
-		}
-		fmt.Fprintf(w, "Welcome to the home page!")
-	})
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
 }
